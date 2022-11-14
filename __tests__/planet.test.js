329409a7b3687f0218c2e38e08d125abffdeccdf
Vera Weikel <veraweikel@Veras-MacBook-Pro-3.local>
2022-11-14 13:03:09 -0800

    It will start to get all the tests to pass for age. Test 1 passes.

--- __tests__/planet.test.js
+++ __tests__/planet.test.js
@@ -7,29 +7,30 @@ describe('Planet', () => {
     planet = new Planet();
   });
 
-  test('should create correctly a planet object',() => {
-    planet = new Planet(1, 10);
-    expect(planet.entry).toEqual(1, 10);
+  test('should correctly create a planet object',() => {
+    let planet = new Planet(10);
+    // planet.age = 10;
+    expect(planet.age).toEqual(10);
   }); 
 
-  test('should return a 1 equivalent to 1 Earth year', () => {
-    planet = new Planet(1, 10);
-    expect(planet.entry).toEqual(1, 10);
-  });
+  // test('should return a 10 equivalent to 1 Earth year', () => {
+  //   planet = new Planet(10);
+  //   expect(planet.entry).toEqual(10);
+  // });
 
-  test('should correctly determine if a planet has been entered', () => {
-    planet = new Planet("Pluto", 10);
-    expect(planet.getPlanetYear()).not.toBeNull();
-  });
+  // test('should correctly determine if a planet has been entered', () => {
+  //   planet = new Planet(10);
+  //   expect(planet.getPlanetYear()).not.toBeNull();
+  // });
 
-  test('should return a 1, equivalent if it is the planet Earth, for 1 Earth year', () => {
-    planet = new Planet("Earth", 10);
-    expect(planet.getPlanetYear()).toEqual(1, 10);
-  });
+  // test('should return 10 equivalent if it is the planet Earth, for 1 Earth year', () => {
+  //   planet = new Planet(10);
+  //   expect(planet.getPlanetYear()).toEqual(10);
+  // });
 
-  test('should return a 10, given Earth, if calculation is equivalent for 1 Earth year', () => {
-    planet = new Planet("Earth", 10);
-    expect(planet.getPlanetYear()).toEqual(10);
-  });
+  // test('should return a 10, given Earth, if calculation is equivalent for 1 Earth year', () => {
+  //   planet = new Planet("Earth", 10);
+  //   expect(planet.getPlanetYear()).toEqual(10);
+  // });
 
 }); 
