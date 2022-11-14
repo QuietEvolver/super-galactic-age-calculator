2caee8b4f530ef71f5743ff2ff4cc9d1c495d70a
Vera Weikel <veraweikel@Veras-MacBook-Pro-3.local>
2022-11-14 11:44:15 -0800

    It will update getPlanetYear for Earth year calculation.

--- src/js/planet.js
+++ src/js/planet.js
@@ -5,7 +5,9 @@ export default class Planet {
   }
 
   getPlanetYear() {
+    let planetYear;
     if(this.entry === 'Earth'){
+      planetYear = ((1/1)*365);
       return 1;
     } else {
       return "Planet out of galactic range."
