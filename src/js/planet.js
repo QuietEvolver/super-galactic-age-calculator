606f84eab349d7a3af73d03232c85a47b8faef5c
Vera Weikel <veraweikel@Veras-MacBook-Pro-3.local>
2022-11-14 18:54:02 -0800

    It will add obj js.

--- src/js/planet.js
+++ src/js/planet.js
@@ -6,23 +6,6 @@ export default class Planet {
   }
 
   getPlanetYear() {
-    // let planetYear;
-    let planetEarth = 'Earth';
-    // this.age = 10;
-    if( planetEarth === 'Earth'){
-      let planetYear = (365/365);
-      this.age *= planetYear;
-      return this.age;
-    // } else {
-    //   return "Planet age out of galactic range.";
-    }
+ 
   }
 }
-  
-// getPlanetYear() {
-//   if(this.entry === 'Earth'){
-//     return 1;
-//   } else {
-//     return "Planet out of galactic range."
-//   }
-// }
