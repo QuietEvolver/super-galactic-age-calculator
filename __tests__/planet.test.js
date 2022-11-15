8290b82a39f5e3f4d767aecb3e9febfa0339ec9c
Vera Weikel <veraweikel@Veras-MacBook-Pro-3.local>
2022-11-14 19:16:31 -0800

    It will add getMercuryPlanetYear test.

--- __tests__/planet.test.js
+++ __tests__/planet.test.js
@@ -27,10 +27,10 @@ describe('Planet', () => {
     expect(planet.getPlanetYear()).toEqual(10);
   });
 
-  // test('should return a 10, given Earth, if calculation is equivalent for 1 Earth year', () => {
-  //   planet = new Planet(10);
-  //   expect(planet.getPlanetYear()).toEqual(10);
-  // });
+  test('should return a 10, given Earth, if calculation is equivalent for 1 Earth year', () => {
+    planet = new Planet(10);
+    expect(planet.getPlanetYear()).toEqual(10);
+  });
 
   // test('should return age for planet Mercury for 10 earth years ', () => {
   //   planet = new Planet(10);
