10368756876ef72f69b75af71ce92c77653f57c0
Vera Weikel <veraweikel@Veras-MacBook-Pro-3.local>
2022-11-21 03:39:55 -0800

    It will add first test with 2 addl params.

--- __tests__/planet.test.js
+++ __tests__/planet.test.js
@@ -1,5 +1,5 @@
 import Planet from './../src/js/planet.js';
-// "I run, fall, rerun\nFall, get back up, and then I\ndrink from water cup"
+ 
 describe('Planet', () => {
   // let planet;
   
@@ -8,51 +8,51 @@ describe('Planet', () => {
   // });
 
   test('should correctly create a planet object',() => {
-    let planet = new Planet(10);
+    let planet = new Planet(10, 0, 0);
     expect(planet.age).toEqual(10);
   }); 
 
-  test('should return a 10 equivalent to 1 Earth year', () => {
-    let planet = new Planet(10);
-    expect(planet.age).toEqual(10);
-  });
+  // test('should return a 10 equivalent to 1 Earth year', () => {
+  //   let planet = new Planet(10);
+  //   expect(planet.age).toEqual(10);
+  // });
 
-  test('should correctly determine if an age has been entered', () => {
-    let planet = new Planet("Pluto");
-    expect(planet.getEarthPlanetYear()).not.toBeNull();
-  });
+  // test('should correctly determine if an age has been entered', () => {
+  //   let planet = new Planet("Pluto");
+  //   expect(planet.getEarthPlanetYear()).not.toBeNull();
+  // });
 
-  test('should return 10 equivalent if it is the planet Earth, for 1 Earth year', () => {
-    let planet = new Planet(10);
-    expect(planet.getEarthPlanetYear()).toEqual(10);
-  });
+  // test('should return 10 equivalent if it is the planet Earth, for 1 Earth year', () => {
+  //   let planet = new Planet(10);
+  //   expect(planet.getEarthPlanetYear()).toEqual(10);
+  // });
 
-  test('should return age for planet Mercury for 10 earth years ', () => { //  876
-    let planet = new Planet(10);
-    expect(planet.getMercuryPlanetYear()).toEqual(42);
-  });
+  // test('should return age for planet Mercury for 10 earth years ', () => { //  876
+  //   let planet = new Planet(10);
+  //   expect(planet.getMercuryPlanetYear()).toEqual(42);
+  // });
 
-  test('should return age for planet Venus for 10 earth years ', () => {
-    let planet = new Planet(10);
-    expect(planet.getVenusPlanetYear()).toEqual(16);
-  });
+  // test('should return age for planet Venus for 10 earth years ', () => {
+  //   let planet = new Planet(10);
+  //   expect(planet.getVenusPlanetYear()).toEqual(16);
+  // });
 
-  test('should return age for planet Mars for 10 earth years ', () => {
-    let planet = new Planet(10);
-    expect(planet.getMarsPlanetYear()).toEqual(5);
-  });
+  // test('should return age for planet Mars for 10 earth years ', () => {
+  //   let planet = new Planet(10);
+  //   expect(planet.getMarsPlanetYear()).toEqual(5);
+  // });
 
-  test('should return age for planet Jupiter for 10 earth years ', () => {
-    let planet = new Planet(10);
-    expect(planet.getJupiterPlanetYear()).toEqual(1);
-  });
+  // test('should return age for planet Jupiter for 10 earth years ', () => {
+  //   let planet = new Planet(10);
+  //   expect(planet.getJupiterPlanetYear()).toEqual(1);
+  // });
 
-  // simple: have the user input the average life expectancy for their demographic
+  // // simple: have the user input the average life expectancy for their demographic
 
-  test('should return a value of +1 a person based on their demographic', () => {
-    planet = new Planet(10);
-    expect(planet.getLifeExpectancy()).toEqual(11);
-  });
+  // test('should return a value of +1 a person based on their demographic', () => {
+  //   planet = new Planet(10);
+  //   expect(planet.getLifeExpectancy()).toEqual(11);
+  // });
 
 //   // const absoluteValue = require('./index');
 //   // test('absolute Value function exists', () => {
--- src/js/planet.js
+++ src/js/planet.js
@@ -1,169 +1,57 @@
 export default class Planet {
   
-  constructor(age) { 
+  constructor() { 
     /*
     this.age = age;
-    this.tabulations = tabulations;
+    
+    this.age = age;
+    this.tabulations = tabulations; // aka demographics
     this.updattedAgeLE = 0;
     
     */
-    this.age = age;
   }
 
-  getEarthPlanetYear() {
-    let ageInEarthDays = this.age * 365;
-    let planetYearInEarthDays = (365);
-    this.age = Math.round(ageInEarthDays/planetYearInEarthDays);
-    console.log("Earth age;", this.age);
-    return this.age;
-  }
+//   getEarthPlanetYear() {
+//     let ageInEarthDays = this.age * 365;
+//     let planetYearInEarthDays = (365);
+//     this.age = Math.round(ageInEarthDays/planetYearInEarthDays);
+//     console.log("Earth age;", this.age);
+//     return this.age;
+//   }
 
-  getMercuryPlanetYear(){
-    // let planetYear = (365/.24)
-    let planetMercuryYearInEarthDays = (87.6);
-    let ageInEarthDays = this.age * 365;
-    this.age = Math.round(ageInEarthDays/planetMercuryYearInEarthDays);
-    console.log("Mercury age;", this.age);
-    return this.age;
-  }
+//   getMercuryPlanetYear(){
+//     // let planetYear = (365/.24)
+//     let planetMercuryYearInEarthDays = (87.6);
+//     let ageInEarthDays = this.age * 365;
+//     this.age = Math.round(ageInEarthDays/planetMercuryYearInEarthDays);
+//     console.log("Mercury age;", this.age);
+//     return this.age;
+//   }
   
-  getVenusPlanetYear(){
-    let planetYearInEarthDays = (226.3);
-    let ageInEarthDays = this.age * 365;
-    this.age = Math.round(ageInEarthDays/planetYearInEarthDays);
-    console.log("Venus age", this.age);
-    return this.age;
-  }
+//   getVenusPlanetYear(){
+//     let planetYearInEarthDays = (226.3);
+//     let ageInEarthDays = this.age * 365;
+//     this.age = Math.round(ageInEarthDays/planetYearInEarthDays);
+//     console.log("Venus age", this.age);
+//     return this.age;
+//   }
 
-  getMarsPlanetYear(){
-    let planetMarsYearInEarthDays = (686.2);
-    let ageInEarthDays = this.age * 365;
-    console.log("Mars ageInEarthDays: ", ageInEarthDays);
-    this.age = Math.round(ageInEarthDays/planetMarsYearInEarthDays);
-    console.log("Mars Years age", this.age);
-    return this.age;
-  }
+//   getMarsPlanetYear(){
+//     let planetMarsYearInEarthDays = (686.2);
+//     let ageInEarthDays = this.age * 365;
+//     console.log("Mars ageInEarthDays: ", ageInEarthDays);
+//     this.age = Math.round(ageInEarthDays/planetMarsYearInEarthDays);
+//     console.log("Mars Years age", this.age);
+//     return this.age;
+//   }
 
-  getJupiterPlanetYear(){
-    let planetJupiterYearInEarthDays = (4328.9);
-    let ageInEarthDays = this.age * 365;
-    let planetYear = ageInEarthDays/planetJupiterYearInEarthDays;
-    console.log("Jupiter planetYear: ", planetYear);
-    this.age = Math.round(planetYear);
-    console.log("Jupiter age: ", this.age);
-    return this.age;
-  }
-// }
-//  getLifeExpectancy(){
-//   const avgLifeExpectancy = 81;  
-//   let lifeExpectancy = avgLifeExpectancy - this.age;
-//   console.log("Avg LE Earth age;", lifeExpectancy);
-//   // return lifeExpectancy;
+//   getJupiterPlanetYear(){
+//     let planetJupiterYearInEarthDays = (4328.9);
+//     let ageInEarthDays = this.age * 365;
+//     let planetYear = ageInEarthDays/planetJupiterYearInEarthDays;
+//     console.log("Jupiter planetYear: ", planetYear);
+//     this.age = Math.round(planetYear);
+//     console.log("Jupiter age: ", this.age);
+//     return this.age;
+//   }
 //  }
-
-// getLifeExpectancy(){
-// // this.age = 50;
-// const tabulations = []; 
-// console.log("tabulations: ", tabulations);
-// tabulations.forEach(demographicQues1 => {
-//   if(demographicQues1 === true){
-//     tabulations.push(1);
-//   } 
-
-
-// adding to age
-// if ( tabulations.length > 0){ 
-//   let newAge = tabulations.length + age; 
-//   console.log("tabPlusAge: ", newAge); // anyT
-// }       
-// return this.age;
-//   });
-// }
-  
-//   // const planet = new Planet(age, this.tabulations);
-//   new Planet(age, this.tabulations);
-
-//   if(document.getElementById("pollen").checked===true){
-//     tabulations.push(1);
-//   } 
-//   if(document.getElementById("moss").checked===true){
-//     tabulations.push(1);
-//   }
-//   if(document.getElementById("azure").checked===true){
-//     tabulations.push(1);
-//   } 
-
-//   if ( tabulations.length > 0){ 
-    
-//     let anyT = tabulations.length + age; 
-//     console.log("tabPlusAge: ", anyT);
-
-//   }  
-//   let anyT = tabulations.length + age; 
-//   console.log ( "age+t", anyT);
-//   let lifeExpectancy = 81;
-//   let calcLifeExpectancy = lifeExpectancy - anyT;
-//   console.log ( "calcLifeExpectancy+t", calcLifeExpectancy);
-// }
-
-// getLifeExpectancy(){
-//   getMercuryPlanetYear();
-//   let lifeExpectancyInEarthYears = 70; // - this.age;answer1
-//   this.age -= lifeExpectancyInEarthYears;
-//   console.log("this.age - LE: ", this.age);
-//   let lifeExpectancy = lifeExpectancyInEarthYears - this.age; // if(lifeExpectancyInEarthYears < 20){} // ABSVAL you can expect to live this much longer
-//   // if(lifeExpectancyInEarthYears > 20){} // you have Lived this much longer than expected
-//   console.log("life expectancy: ", lifeExpectancy);
-//   //const lifeExpectancyCalc = beingLikesFlowers? this.age += 2 
-//   //: beingChewsGum? this.age -1
-//   //  : beingSingsOutLoud? this.age +1
-//   //  : lifeExpectancyInEarthYears + 0;
-  
-//   let output =  this.age;
-//   //let output =  lifeExpectancy - lifeExpectancyCalc;
-//   console.log("output: ", output);
-//   `Howdy, your life expectancy is ${output}`;
-//   // if(answer==='yes'){
-//   //  let age =  this.age+2;
-//   //  return age;
-//   // // } else {
-//   // //   return this.age + 0;
-//   // }
-
-// }
-
-//   getLifeExpectancy(){
-//     let demo = [];
-//     let demoQuestion1 = true;
-//     if(demoQuestion1 === true){
-//       demo.push(1);
-//     } else {
-//       return this.age;
-//     }
-//   }
-// }
-
-
-// getLifeExpectancy(){
-//   let earthLifeExpectancy = 70;
-//   let lifeExpectancy;
-//   let age = Planet.age;
-//   // Planet.tabluations = this.tabluations;
-  
-//   if(this.tabulations.length === 0){
-//     lifeExpectancy = earthLifeExpectancy-age;
-//     // 
-//     return lifeExpectancy;
-//   }
-//   else if(this.size === "pollen"){
-//     age =  age + (this.tabulations.length+1);
-//     return age;
-//   } else if(this.size === "moss"){
-//     age  =  age + (this.tabulations.length+1);
-//     return age;
-//   } else if(this.size === "azure"){
-//     age  =  age + (this.tabulations.length+2);
-//     return age;
-//   }
-// }
-// }
