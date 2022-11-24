e23b6f29a6e41f514ff377ce573cca3e57487cbc
Vera Weikel <veraweikel@Veras-MacBook-Pro-3.local>
2022-11-24 14:29:22 -0800

    It will pass test for planet venus equality.

--- src/js/planet.js
+++ src/js/planet.js
@@ -32,12 +32,12 @@ export default class Planet {
   }
   
   getVenusPlanetYear(){
-//     let planetYearInEarthDays = (226.3);
-//     let ageInEarthDays = this.age * 365;
-//     this.age = Math.round(ageInEarthDays/planetYearInEarthDays);
-//     this.tabulations = [];
-//     console.log("Venus age", this.age);
-//     return this.age;
+    let planetYearInEarthDays = (226.3);
+    let ageInEarthDays = this.age * 365;
+    this.age = Math.round(ageInEarthDays/planetYearInEarthDays);
+    this.tabulations = [];
+    console.log("Venus age", this.age);
+    return this.age;
   }
 
 //   getMarsPlanetYear(){
